0542c35075737e77c1b0fcf29db638939162a09b
Arnav Chhabra <arnav.chh@gmail.com>
2018-04-20 21:25:34 +0530

    Result and analysis

--- results.php
+++ results.php
@@ -1,28 +1,24 @@
 <?php
-session_start();
 $name=$_GET['q'];
 $conn = mysqli_connect("localhost", "root", "", "subdata");
 	if(!$conn)
 		echo mysqli_connect_error();
 	else
 	{
-		query='select Dish_ID Dish_Name Spicy Cuisine pic_name from dish,details where dish.Dish_ID=details.Dish_ID and Dish_Name LIKE '%$name%'';
+		$query="SELECT dish.Dish_ID , Dish_Name , Spicy ,Cuisine , pic_name FROM details,dish WHERE dish.Dish_ID=details.d_id AND Dish_Name LIKE '%$name%' ";
 		$res = mysqli_query($conn, $query);
-		if($res = mysqli_query($conn, $query))
-		{	
-			$array= array();
-			if(mysqli_num_rows($res) > 0)
+			
+		$store=  array();
+		if(mysqli_num_rows($res) > 0)
+		{
+			while($row=mysqli_fetch_assoc($res))
 			{
-				while($row=mysqli_fetch_assoc($res))
-				{
-					$file = file_get_contents("uploads/".$row['pic_name']);
-					$row['pic_name']=base64_encode($file);
-					array_push($array, $row);
-				}
-				$jsonarray=JSON_encode($array);
-				echo $jsonarray;
+				//$file = file_get_contents("uploads/".$row['pic_name']);
+				//$row['pic_name']=base64_encode($file);
+				array_push($store,$row);
 			}
-				
+			$jsonarray=JSON_encode($store);
+			echo $jsonarray;
 		}
 		else
 			echo "Error: " . $query . "<br>" . mysqli_error($conn);
