251f4346c97322fc886afc132f4a86d1e6321203
anishmrao <32713020+anishmrao@users.noreply.github.com>
2018-04-21 19:39:32 +0530

    results

--- results.php
+++ results.php
@@ -5,16 +5,16 @@ $conn = mysqli_connect("localhost", "root", "", "subdata");
 		echo mysqli_connect_error();
 	else
 	{
-		$query=" SELECT dish.Dish_ID , Dish_Name , Spicy ,Cuisine , pic_name FROM details,dish WHERE dish.Dish_ID=details.d_id AND Dish_Name LIKE '%$name%' ";
+		$query="SELECT dish.Dish_ID , Dish_Name , Spicy ,Cuisine , pic_name FROM details,dish WHERE dish.Dish_ID=details.d_id AND Dish_Name LIKE '%$name%' ";
 		$res = mysqli_query($conn, $query);
-			
+
 		$store=  array();
 		if(mysqli_num_rows($res) > 0)
 		{
 			while($row=mysqli_fetch_assoc($res))
 			{
 
-				$file = file_get_contents("uploads/".$row['pic_name']);
+				$file = file_get_contents("newsubs/uploads/".$row['pic_name']);
 				$row['pic_name']=base64_encode($file);
 				array_push($store,$row);
 			}
