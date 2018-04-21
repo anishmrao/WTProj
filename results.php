2e51cb0ef1d05fe9b5b5cefc392d1178170e8edd
Arnav Chhabra <arnav.chh@gmail.com>
2018-04-21 19:32:34 +0530

    Results

--- results.php
+++ results.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect("localhost", "root", "", "subdata");
 		echo mysqli_connect_error();
 	else
 	{
-		$query="SELECT dish.Dish_ID , Dish_Name , Spicy ,Cuisine , pic_name FROM details,dish WHERE dish.Dish_ID=details.d_id AND Dish_Name LIKE '%$name%' ";
+		$query=" SELECT dish.Dish_ID , Dish_Name , Spicy ,Cuisine , pic_name FROM details,dish WHERE dish.Dish_ID=details.d_id AND Dish_Name LIKE '%$name%' ";
 		$res = mysqli_query($conn, $query);
 			
 		$store=  array();
@@ -13,10 +13,12 @@ $conn = mysqli_connect("localhost", "root", "", "subdata");
 		{
 			while($row=mysqli_fetch_assoc($res))
 			{
-				//$file = file_get_contents("uploads/".$row['pic_name']);
-				//$row['pic_name']=base64_encode($file);
+
+				$file = file_get_contents("uploads/".$row['pic_name']);
+				$row['pic_name']=base64_encode($file);
 				array_push($store,$row);
 			}
+			print_r($store);
 			$jsonarray=JSON_encode($store);
 			echo $jsonarray;
 		}
